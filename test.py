15173c54ed78514e6701c5cc79aba917e78d081e
samukolov <>
2024-10-25 18:53:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
-root = ttk.Window()
+# traditional approach
+root = ttk.Tk()
+style = ttk.Style("darkly")
+
+# new approach
+root = ttk.Window(themename="darkly")
 
 b1 = ttk.Button(root, text="Button 1", bootstyle=SUCCESS)
 b1.pack(side=LEFT, padx=5, pady=10)
